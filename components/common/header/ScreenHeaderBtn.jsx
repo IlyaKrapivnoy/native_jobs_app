ada12b7e47dcbe23bdef925d51c19ebd439a4e67
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-26 17:05:25 +0100

    set header btns

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -3,10 +3,14 @@ import { TouchableOpacity, Image } from 'react-native';
 
 import styles from './screenheader.style';
 
-const ScreenHeaderBtn = ({ iconUrl }) => {
+const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress }) => {
   return (
-    <TouchableOpacity>
-      <Image source={iconUrl} />
+    <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
+      <Image
+        source={iconUrl}
+        resizeMode="cover"
+        style={styles.btnImg(dimension)}
+      />
     </TouchableOpacity>
   );
 };
