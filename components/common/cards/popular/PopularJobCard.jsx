31521b6d2b077a92eb9f5d1a5273998eedb24118
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-27 13:04:50 +0100

    add logos to popular cards

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 
-import styles from './popularjobcard.style'
+import styles from './popularjobcard.style';
 
-const PopularJobCard = () => {
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
-  )
-}
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image
+          source={{
+            uri: item?.employer_logo
+              ? item.employer_logo
+              : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg',
+          }}
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+    </TouchableOpacity>
+  );
+};
 
-export default PopularJobCard
+export default PopularJobCard;
